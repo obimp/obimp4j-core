32566a93ce5076fb9c2dcb20b48b64a8d623d951
Alexander Krysin <alessandro56@yandex.ru>
2022-05-27 19:45:54 +0500

    Update package and publishing configuration.

---
+++ src/main/java/io/github/obimp/packet/Packet.java
@@ -0,0 +1,117 @@
+/*
+ * OBIMP4J - Java OBIMP Lib
+ * Copyright (C) 2013—2022 Alexander Krysin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.obimp.packet;
+
+import io.github.obimp.data.structure.sTLD;
+import io.github.obimp.data.structure.wTLD;
+import io.github.obimp.data.DataStructure;
+
+import java.util.HashMap;
+
+/**
+ * Packet
+ * @author Alexander Krysin
+ */
+public class Packet {
+    private byte type;
+    private byte subtype;
+    private HashMap data = new HashMap<Integer, Byte>();
+    private int c = 0; 
+    
+    public Packet(int type, int subtype) {
+        this.type = (byte) type;
+        this.subtype = (byte) subtype;
+        
+        data.put(0, 0x23);
+        data.put(1, 0x00);
+        data.put(2, 0x00);
+        data.put(3, 0x00);
+        data.put(4, 0x00);
+        data.put(5, 0x00);
+        data.put(6, this.type);
+        data.put(7, 0x00);
+        data.put(8, this.subtype);
+        data.put(9, 0x00);
+        data.put(10, 0x00);
+        data.put(11, 0x00);
+        data.put(12, 0x00);
+        data.put(13, 0x00);
+        data.put(14, 0x00);
+        data.put(15, 0x00);
+        data.put(16, 0x00);
+        c = 17;
+    }
+    
+    public int getType() {
+        return type;
+    }
+
+    public int getSubType() {
+        return subtype;
+    }
+
+    public byte[] asByteArray(int seq) {     
+        byte[] bseq = getBytes(seq);
+        data.put(1, bseq[3]);
+        data.put(2, bseq[2]);
+        data.put(3, bseq[1]);
+        data.put(4, bseq[0]);
+        data.put(9, bseq[3]);
+        data.put(10, bseq[2]);
+        data.put(11, bseq[1]);
+        data.put(12, bseq[0]);
+        byte[] size = getBytes(data.size() - 17);
+        data.put(13, size[3]);
+        data.put(14, size[2]);
+        data.put(15, size[1]);
+        data.put(16, size[0]);
+        byte[] p = new byte[data.size()];
+        for(int i=0;i<p.length;i++) {
+            p[i] = Byte.parseByte(String.valueOf(data.get(i)));
+        }
+        
+        return p;
+    }
+    
+    public void append(DataStructure ds) {
+        byte[] d = null;
+        byte[] l = getBytes(ds.getLength());
+        if(ds instanceof wTLD) {
+            d = new byte[] {0x00, 0x00, 0x00, (byte) ds.getType(), l[3], l[2], l[1], l[0]};
+        } else if(ds instanceof sTLD) {
+            d = new byte[] {0x00, (byte) ds.getType(), l[1], l[0]};
+        }
+        for(int i = 0;i<d.length;i++) {
+            data.put(c, d[i]);
+            c++;
+        }
+        for(int i = 0;i<ds.getData().length;i++) {
+            data.put(c, ds.getData()[i]);
+            c++;
+        }
+    }
+    
+    private byte[] getBytes(int x) {
+        byte[] bytes = new byte[4];
+        for (int i = 0; i < bytes.length; i++) {
+           bytes[i] = (byte)(x >> (i * 8));
+        }
+        return bytes;
+    }
+}
---
+++ src/main/java/io/github/obimp/packet/PacketListener.java
@@ -0,0 +1,120 @@
+/*
+ * OBIMP4J - Java OBIMP Lib
+ * Copyright (C) 2013—2022 Alexander Krysin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.obimp.packet;
+
+import io.github.obimp.OBIMPConnection;
+import io.github.obimp.data.structure.wTLD;
+import io.github.obimp.data.type.BLK;
+
+import java.io.DataInputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Vector;
+
+/**
+ * Packet listener
+ * @author Alexander Krysin
+ */
+public class PacketListener implements Runnable {
+    private Socket s;
+    private DataInputStream in;
+    private OBIMPConnection oc;
+    private String username;
+    private String password;
+
+    public PacketListener(Socket s, DataInputStream in, OBIMPConnection oc, String username, String password) {
+        this.s = s;
+        this.in = in;
+        this.oc = oc;
+        this.username = username;
+        this.password = password;
+    }
+    
+    @Override
+    public void run() {
+        Vector<Byte> data = new Vector<>();
+        int k = 0;
+        while(oc.connected) {
+            try {
+                if(k == 17) {
+                    int type = getTypeOrSubtype(data.get(5), data.get(6));
+                    int subtype = getTypeOrSubtype(data.get(7), data.get(8));
+                    int l = getLength(data.get(13), data.get(14), data.get(15), data.get(16));
+                    byte[] body = l > 0 ? new byte[l] : null;
+                    if(body != null)  {
+                        in.readFully(body);
+                        HashMap<Integer, BLK> tlds = parseData(body);
+                        Packet p = new Packet(type, subtype);
+                        for(int i=0;i<tlds.size();i++) {
+                            p.append(new wTLD((int) tlds.keySet().toArray()[i], (BLK) tlds.values().toArray()[i]));
+                        }
+                        PacketHandler.parsePacket(p, tlds, oc, username, password, s);
+                    }
+                    data = new Vector<>();
+                    k = 0;
+                }
+                int i = in.read();
+                data.add((byte) i);
+                k++;
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static int getLength(byte one, byte two, byte three, byte four) {
+        int l;
+        l  = (((int) one) << 24) & 0xFF000000;
+        l |= (((int) two) << 16) & 0x00FF0000;
+        l |= (((int) three) << 8) & 0x0000FF00;
+        l |= (((int) four)) & 0x000000FF;
+        return l;
+    }
+    
+    private int getTypeOrSubtype(byte one, byte two) {
+        int l;
+        l = (((int) one) << 8) & 0x0000FF00;
+        l |= (((int) two)) & 0x000000FF;
+        return l;
+    }
+    
+    private HashMap<Integer, BLK> parseData(byte[] data) {
+        HashMap tlds = new HashMap<Integer, BLK>();
+        while(data.length > 0) {
+            int type = getLength(data[0], data[1], data[2], data[3]);
+            int length = getLength(data[4], data[5], data[6], data[7]);
+            byte[] body = new byte[length];
+            int k = 8;
+            for(int i=0;i<length;i++) {
+                body[i] = data[k];
+                k++;
+            }
+            tlds.put(type, new BLK(body));
+            body = new byte[data.length - (length + 8)];
+            k = length + 8;
+            for(int i=0;i<body.length;i++) {
+                body[i] = data[k];
+                k++;
+            }
+            data = body;
+        }
+        return tlds;
+    }
+    
+}
